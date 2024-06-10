437ca5d4ff596b55fd99b4ac4f0e1e892ca09b5d
adi <tiliztiadi@gmail.com>
2024-06-10 17:56:26 +0700

    fix: template preview

--- resources/views/pages/parts/cv-kerja/portofolio.blade.php
+++ resources/views/pages/parts/cv-kerja/portofolio.blade.php
@@ -14,8 +14,7 @@
     <div class="container">
         <div class="form-group d-flex justify-content-center">
             <a class="btn btn-secondary previous me-2"><i class="fas fa-angle-left"></i> Back</a>
-            {{-- <a class="btn btn-success me-2" onclick="download()"><i class="fas fa-download"></i> Download</a> --}}
-            <a class="btn btn-info" onclick="preview()"><i class="fas fa-eye"></i> Preview</a>
+            <a class="btn btn-info" data-mdb-toggle="modal" data-mdb-target="#modalPilihTemplate"><i class="fas fa-eye"></i> Preview</a>
         </div>
     </div>
 </div>
