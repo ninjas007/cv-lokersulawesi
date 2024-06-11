5b7cd1900fc5a23aa1180a5ade404df76a672689
adi <tiliztiadi@gmail.com>
2024-06-11 14:12:27 +0700

    test

--- resources/views/print/cv-kerja/templates/1.blade.php
+++ resources/views/print/cv-kerja/templates/1.blade.php
@@ -235,6 +235,10 @@
 
 @section('js')
     <script>
+        function isMobileDevice() {
+            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        }
+
         function printAndKeepOpen() {
             window.print();
         }
@@ -244,23 +248,28 @@
 
             // Menangani event saat jendela pencetakan ditutup
             window.onafterprint = function() {
-                // Memberikan jeda sebelum menutup halaman agar jendela pencetakan dapat menyelesaikan prosesnya
-                setTimeout(function() {
-                    // Menutup jendela pencetakan
-                    window.close();
-                }, 100);
+                // Jika pengguna bukan dari perangkat seluler, maka tutup jendela
+                if (!isMobileDevice()) {
+                    // Memberikan jeda sebelum menutup halaman agar jendela pencetakan dapat menyelesaikan prosesnya
+                    setTimeout(function() {
+                        // Menutup jendela pencetakan
+                        window.close();
+                    }, 100);
+                }
             };
 
             // Menangani event saat pencetakan dibatalkan atau disimpan
             window.onbeforeprint = function() {
-                // Memberikan jeda sebelum menutup halaman agar pengguna memiliki kesempatan untuk membatalkan pencetakan
-                setTimeout(function() {
-                    // Menutup jendela pencetakan
-                    window.close();
-                }, 100);
+                // Jika pengguna bukan dari perangkat seluler, maka tutup jendela
+                if (!isMobileDevice()) {
+                    // Memberikan jeda sebelum menutup halaman agar pengguna memiliki kesempatan untuk membatalkan pencetakan
+                    setTimeout(function() {
+                        // Menutup jendela pencetakan
+                        window.close();
+                    }, 100);
+                }
             };
         };
-
 
 
         // document.getElementById('printBtn').onclick = function() {
