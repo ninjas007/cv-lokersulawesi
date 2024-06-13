a202cf64b261ceacd7b6ea9cc5960f86c1bd92a2
adi <tiliztiadi@gmail.com>
2024-06-13 23:47:19 +0700

    fix: fix ui social

--- resources/views/pages/parts/cv-kerja/components/btn-form-inline.blade.php
+++ resources/views/pages/parts/cv-kerja/components/btn-form-inline.blade.php
@@ -1,9 +1,9 @@
-<a href="javascript:void(0)" class="badge badge-pill badge-danger mt-4" onclick="removeFormInline(this)">
+<a href="javascript:void(0)" class="badge badge-pill badge-danger me-2 mt-md-4 mt-sm-3" onclick="removeFormInline(this)">
     <i class="fa fa-times"></i>
 </a>
-<a href="javascript:void(0)" class="badge badge-pill badge-info mt-4" onclick="moveUpFormInline(this)">
+<a href="javascript:void(0)" class="badge badge-pill badge-info me-2 mt-md-4 mt-sm-3" onclick="moveUpFormInline(this)">
     <i class="fa fa-arrow-up"></i>
 </a>
-<a href="javascript:void(0)" class="badge badge-pill badge-info mt-4" onclick="moveDownFormInline(this)">
+<a href="javascript:void(0)" class="badge badge-pill badge-info mt-md-4 mt-sm-3" onclick="moveDownFormInline(this)">
     <i class="fa fa-arrow-down"></i>
 </a>
--- resources/views/pages/parts/cv-kerja/contents/sosial-konten.blade.php
+++ resources/views/pages/parts/cv-kerja/contents/sosial-konten.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row mt-md-0 mt-sm-4">
     <div class="col-md-4 mb-3">
         <label for="sosialMedia" class="mb-1">Nama Sosial Media</label>
         <input type="text" class="form-control custom_validation" name="sosial_media[nama][]" placeholder="Contoh: Github">
@@ -7,7 +7,7 @@
         <label for="link" class="mb-1">Link</label>
         <input type="text" class="form-control custom_validation" name="sosial_media[link][]" placeholder="Contoh: www.github.com/username01">
     </div>
-    <div class="col-md-3 d-flex align-items-center justify-content-between mb-3">
+    <div class="col-md-3 d-flex align-items-center justify-content-start mb-3">
         @include('pages.parts.cv-kerja.components.btn-form-inline')
     </div>
 </div>
