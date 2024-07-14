10b7e44904a991407fa678058180f733f2c1177d
adi <tiliztiadi@gmail.com>
2024-07-14 22:14:08 +0700

    fix: menubar loker

--- resources/views/templates/menubar-footer-loker.blade.php
+++ resources/views/templates/menubar-footer-loker.blade.php
@@ -5,7 +5,7 @@
                 <i class="fa fa-arrow-left"></i> Kembali
             </a>
         @else
-            <a href="{{ url('/lowongan') }}">
+            <a href="{{ url('https://lokersulawesi.com') }}">
                 <button class="btn-menubar">
                     <i class="fa fa-home color-2" style="width: 24px; height: 24px; font-size: 20px"></i>
                     <span class="color-2" style="font-size: 10px">Home</span>
@@ -17,21 +17,12 @@
                     <span class="color-2" style="font-size: 10px">Buat CV</span>
                 </button>
             </a>
-            @if (isset($job))
-                <a href="{{ url('/lowongan?search=true') }}">
-                    <button class="btn-menubar">
-                        <i class="fa fa-search color-2" style="width: 24px; height: 24px; font-size: 20px"></i>
-                        <span class="color-2" style="font-size: 10px">Cari Loker</span>
-                    </button>
-                </a>
-            @else
-                <a href="#" data-toggle="modal" data-target="#filter">
-                    <button class="btn-menubar">
-                        <i class="fa fa-search color-2" style="width: 24px; height: 24px; font-size: 20px"></i>
-                        <span class="color-2" style="font-size: 10px">Cari Loker</span>
-                    </button>
-                </a>
-            @endif
+            <a href="https://lokersulawes.com/artikel">
+                <button class="btn-menubar">
+                    <i class="fa fa-list color-2" style="width: 24px; height: 24px; font-size: 20px"></i>
+                    <span class="color-2" style="font-size: 10px">Artikel</span>
+                </button>
+            </a>
             <a href="{{ url('about') }}">
                 <button class="btn-menubar">
                     <i class="fa fa-comment color-2" style="width: 24px; height: 24px; font-size: 20px"></i>
